773c42dd3cfd9cd2ef93dc283978789af8a11b53
Jannes <jannescb@me.com>
2020-10-27 14:42:06 +0100

    fixing svg in buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
 @if ($type == 'dark')
     <a 
-        class="text-lg inline-flex bg-white hover:bg-black text-black hover:text-white font-normal py-2 px-8 border-2 border-black rounded-sm cursor-pointer " 
+        class="inline-flex items-center px-8 py-2 text-lg font-normal text-black bg-white border-2 border-black rounded-sm cursor-pointer hover:bg-black hover:text-white " 
         href="{{ $link }}" 
         target="{{ $target }}" 
         @if($target == '_blank') rel="noreferrer"@endif>
@@ -8,7 +8,7 @@
     </a>
 @endif
 @if ($type == 'light')
-    <a class="text-lg inline-block bg-black hover:bg-white text-white hover:text-black font-normal py-2 px-8 border-2 border-white rounded-sm cursor-pointer " 
+    <a class="inline-flex items-center px-8 py-2 text-lg font-normal text-white bg-black border-2 border-white rounded-sm cursor-pointer hover:bg-white hover:text-black " 
         href="{{ $link }}" 
         target="{{ $target }}" 
         @if($target == '_blank')rel="noreferrer" @endif>
